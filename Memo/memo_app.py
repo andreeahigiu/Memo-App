ad2c3631ba48379a0d8e788ec9524fd75f40444b
andreeahigiu <higiuandreea@gmail.com>
2021-12-13 00:49:49 +0200

    Open an existing Memo

--- Memo/memo_app.py
+++ Memo/memo_app.py
@@ -40,10 +40,55 @@ c.execute("""CREATE TABLE if not exists memos (
 c.execute("SELECT *, oid FROM memos")
 records = c.fetchall()
 
+#Function to update a Memo
+def update():
+    return
 
 # Function to ope an existing memo
-def open(i):
-    print(i)
+def open_memo(id):
+    print(id)
+    global editor, date, time
+    editor = Tk()  # creating the GUI window
+    editor.title('Update a record')
+    editor.geometry("600x300")
+
+    conn = sqlite3.connect('saved_memos.db')
+    c = conn.cursor()
+
+    record_id = str(id)
+    c.execute("SELECT * FROM memos WHERE oid = " + record_id)
+    records = c.fetchall()
+
+    #Creating Global Variables for text box names
+    global date_edit
+    global time_edit
+    global content_edit
+
+    content_edit = Text(editor, height=10, width=30)
+    content_edit.grid(row=2, column=1)
+
+    for record in records:
+        date = record[0]
+        time = record[1]
+
+    # Creating the text box Labels
+    date_label = Label(editor, text=date)
+    date_label.grid(row=0, column=1)
+    time_label = Label(editor, text="Last edited at:  " + time)
+    time_label.grid(row=1, column=1)
+    content_label = Label(editor, text="Memo Content:")
+    content_label.grid(row=2, column=0)
+
+    # Create a save button:
+    save_btn = Button(editor, text="Save", command=update)
+    save_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=135)
+
+    # Loop through the results:
+    for record in records:
+        content_edit.insert('1.0', record[2])
+
+    conn.commit()
+    conn.close()
 
 
 def show():
@@ -53,7 +98,6 @@ def show():
     # Query the db
     c.execute("SELECT *, oid FROM memos")
     records = c.fetchall()
-    # print(records)
 
     print_rec = ''
     for record in records:
@@ -69,10 +113,9 @@ def show():
 #Showing all the existing notes
 count=2
 for record in records:
-    current_rec = str(record[3]) + " " + str(record[0]) + " " + str(record[1]) + " " + str(record[2]).split()[0] + "\n"
+    current_rec = str(record[3]) + " " + str(record[0]) + " " + str(record[2]).split()[0] + "\n"
     current_id = record[3]
-    print(current_rec[0])
-    query_btn = Button(root, text=current_rec, height=2, width=100, command=partial(open, current_id))
+    query_btn = Button(root, text=current_rec, height=2, width=100, command=partial(open_memo, current_id))
     query_btn.grid(row=count, column=0, columnspan=3, pady=10, padx=10)
     count += 1
 
