32050a4b7c690374002844dc6e857d5372065ee8
andreeahigiu <higiuandreea@gmail.com>
2021-12-13 00:09:00 +0200

    Created DB and show elements
    Created database and displayed the elements that exist in it

--- Memo/memo_app.py
+++ Memo/memo_app.py
@@ -22,10 +22,68 @@ logo_label = Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=1, row=0)
 
+# Database:
+
+# Create the database/ Connect to a DB
+conn = sqlite3.connect('saved_memos.db')
+
+# create a cursor:
+c = conn.cursor()
+
+# Creating the table:
+c.execute("""CREATE TABLE if not exists memos (
+        date text,
+        time text,
+        content txt
+        )""")
+
+c.execute("SELECT *, oid FROM memos")
+records = c.fetchall()
+
+
+# Function to ope an existing memo
+def open(i):
+    print(i)
+
+
+def show():
+    conn = sqlite3.connect('saved_memos.db')
+    c = conn.cursor()
+
+    # Query the db
+    c.execute("SELECT *, oid FROM memos")
+    records = c.fetchall()
+    # print(records)
+
+    print_rec = ''
+    for record in records:
+        print_rec += str(record[0]) + " " + str(record[1]) + " " + str(record[2]) + " " + str(record[3]) + "\n"
+
+    query_label = Label(root, text=print_rec)
+    query_label.grid(row=2, column=0, columnspan=2)
+
+    conn.commit()
+    conn.close()
+
+
+#Showing all the existing notes
+count=2
+for record in records:
+    current_rec = str(record[3]) + " " + str(record[0]) + " " + str(record[1]) + " " + str(record[2]).split()[0] + "\n"
+    current_id = record[3]
+    print(current_rec[0])
+    query_btn = Button(root, text=current_rec, height=2, width=100, command=partial(open, current_id))
+    query_btn.grid(row=count, column=0, columnspan=3, pady=10, padx=10)
+    count += 1
+
+# query_btn1 = Button(root, text="shit", height=2, width=100, command=open)
+# query_btn1.grid(row=count, column=0, columnspan=3, pady=10, padx=10)
+#command=lambda: open(i)
+
 
 
 # Create a New memo button
-new_btn= Button(root, text="Create new Memo")
-new_btn.grid(row=3, column=2, columnspan=1, pady=10, padx=10)
+new_btn = Button(root, text="Create new Memo", command= show)
+new_btn.grid(row=count+1, column=2, columnspan=1, pady=10, padx=10)
 
 root.mainloop()
